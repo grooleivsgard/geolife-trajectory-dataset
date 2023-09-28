eb9607d4ec11cd506f1e55713a545645c6800013
Brilleslangen <nicolaiSivesind@gmail.com>
2023-09-28 19:45:42 +0200

    Update

--- dataset.py
+++ dataset.py
@@ -2,40 +2,6 @@ import pandas as pd
 import os
 
 columns = ['lat', 'lon', 'dep1', 'alt', 'date', 'date_str', 'time_str']
-#test = pd.read_table('geolife_ds/Geolife Trajectories 1.3/Data/000/Trajectory/20081023025304.plt', skiprows=7, names=columns, delimiter=',')
-# Sjekk mappe 'Data' og lag rad for hvert mappenavn (= user_id).
-# If user_id er i 'Data/labeled_ids.txt'
-#   user.has_labels = True
-# Gå inn i hver user mappe, og opprett aktivitet i table_name 'Activity' for hver '.plt' fil. Oppdater user_id, start_time, end_time.
-# If user.has_labels = true { 
-#   loop gjennom '/Data/user_id/labels.txt', og sjekk om noen av aktivitetene har eksakt overlapp med start_time og end_time i user.activity
-#       if overlapp
-#           activity.transportation_mode = (transportation mode)
-#       else 
-#           activity.transportation_mode = NULL
-# Else
-#   transportation_mode = NULL
-# For hver .plt fil (1 .plt fil = en aktivitet) i Trajectory where antall rader <= 2500(unntatt headers)
-#   For hver rad i .plt filen
-#       Opprett ny Trackpoint med lat, long, altitude, date_days, date_time (mulig dette kan gjøres på en raskere måte?)
-
-def insert(path):
-    for dirpath, dirname, filenames in os.walk(path):
-        print(f"Found directory: {dirpath}")
-        
-                        
-#activities.append(pd.read_table(path, skiprows=7, names=columns, delimiter=',')
-
-"""
-def process_label(user, activity):
-     labels = []
-     if user.has_label : true
-        for entry in labels.txt
-            if start_time && end_time i labels.txt == start_time && end_time i activities
-                update label i activity.transportation_mode til 'Transportation Mode'
-    else activity.transportation_mode = NULL
-        
-"""
 
 def read_file_to_list(file_path):
     """
@@ -57,34 +23,76 @@ def read_file_to_list(file_path):
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
-    
-def process_users(path):
-    with os.scandir(path) as users:
-        for user in users:
-            if user.is_dir():
-                print(f"Processing user: {user.name}")
-                print(f'')
-                process_trajectories(user.path)
 
-def process_trajectories(sub_dir_path):
-    with os.scandir(sub_dir_path) as entries:
-        for entry in entries:
-            if entry.is_file():
-                print(f"Processing file: {entry.name} in directory: {sub_dir_path}")
-                
-                
-def retrieve_user(user_dir:os.DirEntry[str], labled_ID:list):
+
+def retrieve_user(user_dir: os.DirEntry[str], labled_ID: list):
     labels = 'false'
     if user_dir.name in labled_ID:
         labels = "true"
-    
+
     return {
         "id": user_dir.name,
-        "has_labels": labels
+        "has_labels": labels,
+        "path": user_dir.path
     }
-    
-    
-data_path = './dataset/Data'
-labeled_ids= read_file_to_list('./dataset/labeled_ids.txt')
 
-print(labeled_ids)
+
+def process_users(path, labeled_ids):
+    user_rows = []
+    with os.scandir(path) as users:
+        for user in users:
+            if user.is_dir():
+                user_row = retrieve_user(user, labeled_ids)
+                user_rows.append(user_row)
+
+    # Insert user
+    return user_rows
+
+
+def retrieve_activity(activity_dir: pd.DataFrame, activity_name,  user_row):
+    activity = {
+        "activity_id": user_row["id"] + "-" + activity_name,
+        "user_id": user_row["id"],
+        "start_time": activity_dir['date_str'].iloc[0] + " " + activity_dir['time_str'].iloc[0],
+        "end_time": activity_dir['date_str'].iloc[-1] + " " + activity_dir['time_str'].iloc[-1],
+    }
+
+    # Check for transportation mode
+    if user_row['has_labels'] == "true":
+        transportations = pd.read_table(user_row['path'] + "/labels.txt")
+        for index in transportations.index:
+            activity['transportation_mode'] = transportations['Transportation Mode'].iloc[index] \
+                if (activity['start_time'] == transportations['Start Time'].iloc[index]
+                    and activity['end_time'] == transportations['End Time'].iloc[index]) \
+                else "null"
+    print(activity)
+
+
+def process_activities(user_rows):
+    for user_row in user_rows:
+
+        with os.scandir(user_row['path'] + "/Trajectory") as activities:
+            for activity in activities:
+                if activity.is_file():
+
+                    trackpoints = pd.read_table(activity.path, skiprows=7, names=columns, delimiter=',')
+
+                    # Skip if larger than 2500 trackpoints
+                    if trackpoints.shape[0] > 2500:
+                        continue
+
+                    retrive_activity(trackpoints, activity_name=activity.name,user_row=user_row)
+                    exit()
+
+def retrieve_trackpoints(trackpoints:pd.DataFrame, activity):
+    trackpoint = {'activity_id':
+                  ', 'lat DOUBLE', 'lon DOUBLE',
+                       'altitude INT', 'date_days DOUBLE', 'date_time DATETIME'}
+    for i, rows in trackpoints.iterrows():
+
+
+
+data_path = './dataset/dataset/Data'
+labeled_ids = read_file_to_list('./dataset/dataset/labeled_ids.txt')
+
+process_users(data_path, labeled_ids)
--- execute.py
+++ execute.py
@@ -1,5 +1,6 @@
 from Database import Database
 
+
 # Task1
 # Write a Python database that does the following:
 # 1. Connects to the MySQL server on your Ubuntu virtual machine.
@@ -11,6 +12,7 @@ def open_connection() -> Database:
         print("ERROR: Failed to use database:", e)
     return database;
 
+
 def close_connection(database: Database):
     if database:
         try:
@@ -19,21 +21,21 @@ def close_connection(database: Database):
             print("ERROR: Failed to close database:", e)
     else:
         print("Database is None.")
-    
 
 
 # 2. Creates and define the tables User, Activity and TrackPoint
 def create_tables(database: Database, debug=False):
     # Table dicts
-    user= {
+    user = {
         'name': "User",
-        'attributes': ["id VARCHAR(3) NOT NULL", "has_labels BIT"], 
+        'attributes': ["id VARCHAR(3) NOT NULL", "has_labels BIT"],
         'primary': "id"
     }
 
     activity = {
-        'name' : 'Activity',
-        'attributes': ['id INT NOT NULL AUTO_INCREMENT', 'user_id VARCHAR(3) NOT NULL', 'transportation_mode VARCHAR(30)', 'start_time_date DATETIME', 'end_date_time DATETIME'],
+        'name': 'Activity',
+        'attributes': ['id INT NOT NULL AUTO_INCREMENT', 'user_id VARCHAR(3) NOT NULL',
+                       'transportation_mode VARCHAR(30)', 'start_time_date DATETIME', 'end_date_time DATETIME'],
         'primary': 'id',
         'foreign': {
             'key': 'user_id',
@@ -42,10 +44,11 @@ def create_tables(database: Database, debug=False):
     }
 
     trackpoint = {
-        'name' : 'TrackPoint',
-        'attributes' : ['id INT NOT NULL AUTO_INCREMENT', 'activity_id INT NOT NULL', 'lat DOUBLE', 'lon DOUBLE', 'altitude INT', 'date_days DOUBLE', 'date_time DATETIME'],
-        'primary' : "id",
-        'foreign' : {
+        'name': 'TrackPoint',
+        'attributes': ['id INT NOT NULL AUTO_INCREMENT', 'activity_id INT NOT NULL', 'lat DOUBLE', 'lon DOUBLE',
+                       'altitude INT', 'date_days DOUBLE', 'date_time DATETIME'],
+        'primary': "id",
+        'foreign': {
             'key': 'activity_id',
             'references': 'Activity(id)'
         }
@@ -53,8 +56,11 @@ def create_tables(database: Database, debug=False):
 
     # Execute queries for creating tables
     database.create_table(user['name'], user['attributes'], user['primary'], debug=debug)
-    database.create_table(activity['name'], activity['attributes'], activity['primary'], activity['foreign'], debug=debug)
-    database.create_table(trackpoint['name'], trackpoint['attributes'], trackpoint['primary'], trackpoint['foreign'], debug=debug)
+    database.create_table(activity['name'], activity['attributes'], activity['primary'], activity['foreign'],
+                          debug=debug)
+    database.create_table(trackpoint['name'], trackpoint['attributes'], trackpoint['primary'], trackpoint['foreign'],
+                          debug=debug)
+
 
 database = open_connection()
 create_tables(database, debug=True)
@@ -63,5 +69,3 @@ close_connection(database)
 # 3. Inserts the data from the Geolife dataset into your MySQL database
 
 # Execution
-
-
