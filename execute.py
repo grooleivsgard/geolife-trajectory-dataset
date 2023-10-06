be99df6c0d1834721802e78d9b3c1f940ae8ce02
Brilleslangen <nicolaiSivesind@gmail.com>
2023-10-06 19:24:59 +0200

    removed old output file

--- execute.py
+++ execute.py
@@ -5,6 +5,11 @@ from part2 import Part2
 part1 = Part1()
 part1.upload_data()
 
+# Perform queries to database
 part2 = Part2()
 part2.execute_tasks(task_nums=1)
+# OR
+part2.execute_tasks(task_nums=[1, 2, 6, 9])
+# OR
+part2.execute_tasks(task_nums=range(1, 13))  # 13 is exclusive
 
